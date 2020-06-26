1fe1082470fbe07f42ba20f741e72ddc105cfa3f
doguz <doguz@airspan.com>
2020-06-26 21:51:57 +0300

    added factory

--- rf_calculator/libs/__init__.py
+++ rf_calculator/libs/__init__.py
@@ -9,18 +9,23 @@ from .packet_size import PacketSize, BitrateFormat
 ERROR_INFO = namedtuple('ERROR_INFO', ['File', 'Line'])
 
 
+def format_factory(string: str):
+    try:
+        return PacketSize(string)
+    except ValueError:
+        try:
+            return RobotTime(string)
+        except ValueError:
+            try:
+                return Percent(string)
+            except Exception as e:
+                raise ValueError(f"Cannot cast object type for '{string}'")
+
+
 def get_error_info():
     exc_type, exc_obj, exc_tb = sys.exc_info()
     file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     return ERROR_INFO(File=file_name, Line=exc_tb.tb_lineno)
-
-
-def format_factory(string: str):
-    if string.endswith('%'):
-        return Percent(string)
-    if len([0 for i in BitrateFormat if string.endswith(i.name)]) > 0:
-        return PacketSize(string)
-    return RobotTime(string)
 
 
 __all__ = [
