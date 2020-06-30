82f0a9939c6bbc3b0f92af84b3c5a371e175231a
doguz <doguz@airspan.com>
2020-06-30 10:51:44 +0300

    Ver: 2.3.b: open branch 'development'

--- robot_math/__init__.py
+++ robot_math/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = '2.3'
+__version__ = '2.4'
 __author__ = 'Dmitry Oguz'
 __author_email__ = 'doguz2509@gmail.com'
 __git_url__ = r'https://github.com/doguz2509/robotframework-calculator'
